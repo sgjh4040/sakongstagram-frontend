98f2ab4b24d61b3c91bf9a292b48f207e140a7cd
sgjh4040 <sgjh4040@naver.com>
2019-10-03 17:19:54 +0900

    localState

---
+++ src/Apollo/Client.js
@@ -0,0 +1,10 @@
+import ApolloCient from 'apollo-boost';
+import {defaults, resolvers} from "./LocalState";
+
+export default new ApolloCient({
+    uri:"http://localhost:4000/",
+    clientState:{
+        defaults,
+        resolvers
+    }
+});
--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
-
-import {ThemeProvider} from "styled-components";
+import {gql} from "apollo-boost";
+import { ThemeProvider } from "styled-components";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
 import Router from "./Router";
+import { useQuery } from 'react-apollo-hooks';
 
-export default ()=>(
-  <ThemeProvider theme={Theme}>
-  <>  
-    <GlobalStyles/>
-    <Router isLoggedIn={true}></Router>
-  </>
+const QUERY = gql`
+  {
+    isLoggedIn @client
+
+  }
+`
+export default () => {
+  const {data:{isLoggedIn}} =useQuery(QUERY);
+
+  return (
+    <ThemeProvider theme={Theme}>
+    <>
+      <GlobalStyles />
+      <Router isLoggedIn={isLoggedIn}></Router>
+    </>
   </ThemeProvider>
-)
+  )
+}
