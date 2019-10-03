634d6186cd3c9aa2b9535486bfcca212229101aa
sgjh4040 <sgjh4040@naver.com>
2019-10-03 16:30:44 +0900

    update routes

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,9 +3,13 @@ import React from 'react';
 import {ThemeProvider} from "styled-components";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
+import Router from "./Router";
 
 export default ()=>(
   <ThemeProvider theme={Theme}>
+  <>  
     <GlobalStyles/>
+    <Router isLoggedIn={true}></Router>
+  </>
   </ThemeProvider>
 )
---
+++ src/Components/Router.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {HashRouter as Router,Route,Switch} from "react-router-dom";
+import PropTypes from "prop-types";
+import Auth from "../Routes/Auth"
+import Feed from "../Routes/Feed";
+
+
+const LoggedInRoutes= () => <><Route exact path="/" component={Feed} /></>
+
+const LoggedOutRoutes= () => <><Route exact path="/" component={Auth}/></>
+
+const AppRouter = ({isLoggedIn})=><Router><Switch>{isLoggedIn ?<LoggedInRoutes/>:<LoggedOutRoutes/>}</Switch></Router>
+
+Router.propTypes = {
+    isLoggedIn: PropTypes.bool.isRequired
+};
+
+export default AppRouter;
