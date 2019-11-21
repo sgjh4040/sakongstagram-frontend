ac71bb7fad33c72ecb0bdc8b83140c69158abdf8
jeonghunSakong <sgjh4040@naver.com>
2019-11-21 18:13:26 +0900

    edit post

--- src/Routes/Auth/AuthPresenter.js
+++ src/Routes/Auth/AuthPresenter.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import Input from "../../Components/Input";
 import Button from "../../Components/Button";
+import Helmet from "react-helmet";
 
 const Wrapper = styled.div`
   min-height: 80vh;
@@ -59,26 +60,41 @@ export default ({
   <Wrapper>
     <Form>
       {action === "logIn" && (
+        <>
+        <Helmet>
+          <title>Log In | Prismagram</title>
+        </Helmet>
         <form onSubmit={onSubmit}>
           <Input placeholder={"Email"} {...email} type="email" />
           <Button text={"Log in"} />
         </form>
+      </>
         
       )}
       {action === "signUp" && (
+        <>
+        <Helmet>
+          <title>Sign Up | Prismagram</title>
+        </Helmet>
         <form onSubmit={onSubmit}>
-        <Input placeholder={"First name"} {...firstName} />
-        <Input placeholder={"Last name"} {...lastName} />
-        <Input placeholder={"Email"} {...email} type="email" />
-        <Input placeholder={"Username"} {...username} />
-        <Button text={"Sign up"} />
-      </form>
+          <Input placeholder={"First name"} {...firstName} />
+          <Input placeholder={"Last name"} {...lastName} />
+          <Input placeholder={"Email"} {...email} type="email" />
+          <Input placeholder={"Username"} {...username} />
+          <Button text={"Sign up"} />
+        </form>
+      </>
       )}
       {action === "confirm" && (
+        <>
+        <Helmet>
+          <title>Confirm Secret | Prismagram</title>
+        </Helmet>
         <form onSubmit={onSubmit}>
           <Input placeholder="Paste your secret" required {...secret} />
           <Button text={"Confirm"} />
         </form>
+      </>
       )}
     </Form>
     <StateChanger>
--- src/Routes/Feed.js
+++ src/Routes/Feed.js
@@ -1,1 +1,73 @@
-export default () => "Feed";
+import React from "react";
+import styled from "styled-components";
+import { gql } from "apollo-boost";
+import { useQuery } from "react-apollo-hooks";
+import Loader from "../Components/Loader";
+import Post from "../Components/Post";
+import Helmet from "react-helmet";
+
+const FEED_QUERY = gql`
+  {
+    seeFeed {
+      id
+      location
+      caption
+      user {
+        id
+        avatar
+        username
+      }
+      files {
+        id
+        url
+      }
+      likeCount
+      isLiked
+      comments {
+        id
+        text
+        user {
+          id
+          username
+        }
+      }
+      createdAt
+    }
+  }
+`;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 80vh;
+`;
+
+export default () => {
+  const { data, loading } = useQuery(FEED_QUERY);
+  console.log(data,loading);
+  return (
+    <Wrapper>
+      <Helmet>
+        <title>Feed | sakongStargram</title>
+      </Helmet>
+      {loading && <Loader />}
+      {!loading &&
+        data &&
+        data.seeFeed &&
+        data.seeFeed.map(post => (
+          <Post
+            key={post.id}
+            id={post.id}
+            location={post.location}
+            caption={post.caption}
+            user={post.user}
+            files={post.files}
+            likeCount={post.likeCount}
+            isLiked={post.isLiked}
+            comments={post.comments}
+            createdAt={post.createdAt}
+          />
+        ))}
+    </Wrapper>
+  );
+};
