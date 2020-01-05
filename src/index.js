12eea9b63c6e97482fba68d79631dd3a5d7089ab
jeonghunSakong <sgjh4040@naver.com>
2020-01-05 20:12:15 +0900

    fix ie11 issue

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from './Components/App';
 import { ApolloProvider } from "react-apollo-hooks";
 import Client from './Apollo/Client';
 import 'babel-polyfill';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
 ReactDOM.render(<ApolloProvider client={Client}><App /></ApolloProvider>, document.getElementById('root'));
 
